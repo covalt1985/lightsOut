9dbab9478a9a8e6c693b5c74209480eb6d14cae6
Tomasz Kowalik <tomaszkowalik85@gmail.com>
2021-03-17 19:04:38 +0100

    Updates

---
+++ precache-manifest.38091567ebc2821a4be9e038e429fda9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d932570d55c28ccd2679",
+    "url": "/lightsOut/static/css/main.c3288d45.chunk.css"
+  },
+  {
+    "revision": "d932570d55c28ccd2679",
+    "url": "/lightsOut/static/js/main.d932570d.chunk.js"
+  },
+  {
+    "revision": "a58744228c3d5a8f5c69",
+    "url": "/lightsOut/static/js/1.a5874422.chunk.js"
+  },
+  {
+    "revision": "7bb51e0662d9c55e23aa",
+    "url": "/lightsOut/static/js/runtime~main.7bb51e06.js"
+  },
+  {
+    "revision": "79e7ab1d91b14998636e0563ec8b8eb5",
+    "url": "/lightsOut/index.html"
+  }
+];
